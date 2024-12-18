d4a95b170f9f2a5790be7f9018c52f943fbf9eaf
yingfeng <670422028@qq.com>
2024-12-18 23:24:26 +0800

    成绩管理系统系统 v1.0.0 测试

---
+++ src/main/java/com/scoremg/api/AccountController.java
@@ -0,0 +1,17 @@
+package com.scoremg.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: scoremg
+ * @description:
+ * @author: 陈程猿
+ * @create: 2024-12-18 23:23
+ **/
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+}
